44449f2a7ebcb0a1cf73cdc9bb8c267f4711c350
a.ito <10333049+BoxPistols@users.noreply.github.com>
2025-05-07 23:35:00 +0900

    TaskList.stories.jsxのデコレーターを追加し、タスクの状態を設定

--- src/components/TaskList.stories.jsx
+++ src/components/TaskList.stories.jsx
@@ -5,14 +5,36 @@ export default {
   component: TaskList,
   title: "Components/TaskList",
   tags: ["autodocs"],
+  decoderators: [
+    (Story) => {
+      return (
+        <div style={{padding: "2rem"}}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+  parameters: {
+    layout: "centered",
+  },
 };
 
 export const Default = {
   args: {
     tasks: [
       {...TaskStories.Default.args.task, id: "1", title: "Task 1"},
-      {...TaskStories.Default.args.task, id: "2", title: "Task 2"},
-      {...TaskStories.Default.args.task, id: "3", title: "Task 3"},
+      {
+        ...TaskStories.Default.args.task,
+        id: "2",
+        title: "Task 2",
+        state: "TASK_PINNED",
+      },
+      {
+        ...TaskStories.Default.args.task,
+        id: "3",
+        title: "Task 3",
+        state: "TASK_ARCHIVED",
+      },
     ],
   },
 };
